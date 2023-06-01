d755d6224781636cf3474fbc69ab2caf0a96fd62
Prabin535 <senapatiprabin10@gmail.com>
2023-06-01 19:07:39 +0530

    first commit

---
+++ mi/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def rohit(request):
+    return render(request,"rohit.html")
+
+def sky(request):
+    return render(request,'sky.html')
